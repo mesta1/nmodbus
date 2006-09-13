42f8a1018dbf4b12e1a125d05f0c7e85023510ab
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2006-09-13 15:15:27 +0000

    Refactored some duplicate code to ModbusTransport.

--- NModbus/src/Modbus/IO/ModbusTransport.cs
+++ NModbus/src/Modbus/IO/ModbusTransport.cs
@@ -20,10 +20,9 @@ namespace Modbus.IO
 			throw new Exception("The method or operation is not implemented.");
 		}
 
-		public T UnicastMessage<T>(IModbusMessage request) where T : IModbusMessage, new()
+		public T UnicastMessage<T>(IModbusMessage message) where T : IModbusMessage, new()
 		{
 			T response = default(T);
-
 			int attempt = 1;
 			bool success = false;
 
@@ -31,11 +30,10 @@ namespace Modbus.IO
 			{
 				try
 				{
-					Write(request);
+					Write(message);
 					response = CreateResponse<T>(Read());
 					success = true;
 				}
-
 				catch (Exception ioe)
 				{
 					if (attempt++ >= _retries)
@@ -46,7 +44,20 @@ namespace Modbus.IO
 			return response;
 		}
 
-		public abstract T CreateResponse<T>(byte[] frame) where T : IModbusMessage, new();
+		public virtual T CreateResponse<T>(byte[] frame) where T : IModbusMessage, new()
+		{
+			byte functionCode = frame[1];
+
+			// check for slave exception response
+			if (functionCode > Modbus.ExceptionOffset)
+				throw new SlaveException(ModbusMessageFactory.CreateModbusMessage<SlaveExceptionResponse>(frame));
+
+			// create message from frame
+			T response = ModbusMessageFactory.CreateModbusMessage<T>(frame);
+
+			return response;
+		}
+
 		public abstract byte[] BuildMessageFrame(IModbusMessage message);
 		public abstract byte[] Read();
 		public abstract void Write(IModbusMessage message);
