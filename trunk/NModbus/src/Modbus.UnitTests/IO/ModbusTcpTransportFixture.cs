8a9c1a01dadd53b1c592f8077e8a02c1be82ca9a
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2006-10-05 20:08:14 +0000

    Fixed broken tests.

--- trunk/NModbus/src/Modbus.UnitTests/IO/ModbusTcpTransportFixture.cs
+++ trunk/NModbus/src/Modbus.UnitTests/IO/ModbusTcpTransportFixture.cs
@@ -18,7 +18,7 @@ namespace Modbus.UnitTests.IO
 		public void BuildMessageFrame()
 		{
 			ReadCoilsInputsRequest message = new ReadCoilsInputsRequest(Modbus.ReadCoils, 2, 10, 5);
-			Assert.AreEqual(new byte[] { 0, 0, 0, 0, 0, 5, 2, 1, 0, 10, 0, 5 }, new ModbusTcpTransport().BuildMessageFrame(message));
+			Assert.AreEqual(new byte[] { 0, 0, 0, 0, 0, 6, 2, 1, 0, 10, 0, 5 }, new ModbusTcpTransport().BuildMessageFrame(message));
 		}
 
 		[Test]
@@ -26,7 +26,7 @@ namespace Modbus.UnitTests.IO
 		{
 			WriteMultipleRegistersRequest message = new WriteMultipleRegistersRequest(3, 1, RegisterCollection.CreateRegisterCollection(0, 255));
 			byte[] header = ModbusTcpTransport.GetMbapHeader(message);
-			Assert.AreEqual(new byte[] { 0, 0, 0, 0, 2, 4, 3}, header);
+			Assert.AreEqual(new byte[] { 0, 0, 0, 0, 2, 5, 3}, header);
 		}
 
 		//[Test]
