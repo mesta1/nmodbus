4c3dd169d9443c058c5fb1b75fde06ee4e9738bf
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2007-03-21 14:06:47 +0000

    Adding comments

--- NModbus/src/Modbus/Device/ModbusMaster.cs
+++ NModbus/src/Modbus/Device/ModbusMaster.cs
@@ -17,44 +17,96 @@ namespace Modbus.Device
 		{
 		}
 
+		/// <summary>
+		/// Read from 1 to 2000 contiguous coils status.
+		/// </summary>
+		/// <param name="slaveAddress">Address of device to read values from.</param>
+		/// <param name="startAddress">Address to begin reading.</param>
+		/// <param name="numberOfPoints">Number of coils to read.</param>
+		/// <returns>Coils status</returns>
 		public bool[] ReadCoils(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
 		{
 			return ReadDiscretes(Modbus.ReadCoils, slaveAddress, startAddress, numberOfPoints);
 		}
-			
+
+		/// <summary>
+		/// Read from 1 to 2000 contiguous discrete input status.
+		/// </summary>
+		/// <param name="slaveAddress">Address of device to read values from.</param>
+		/// <param name="startAddress">Address to begin reading.</param>
+		/// <param name="numberOfPoints">Number of discrete inputs to read.</param>
+		/// <returns>Discrete inputs status</returns>
 		public bool[] ReadInputs(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
 		{
 			return ReadDiscretes(Modbus.ReadInputs, slaveAddress, startAddress, numberOfPoints);
 		}		
 
+		/// <summary>
+		/// Read contiguous block of holding registers.
+		/// </summary>
+		/// <param name="slaveAddress">Address of device to read values from.</param>
+		/// <param name="startAddress">Address to begin reading.</param>
+		/// <param name="numberOfPoints">Number of holding registers to read.</param>
+		/// <returns>Holding registers status</returns>
 		public ushort[] ReadHoldingRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
 		{
 			return ReadRegisters(Modbus.ReadHoldingRegisters, slaveAddress, startAddress, numberOfPoints);
 		}
 
+		/// <summary>
+		/// Read contiguous block of input registers.
+		/// </summary>
+		/// <param name="slaveAddress">Address of device to read values from.</param>
+		/// <param name="startAddress">Address to begin reading.</param>
+		/// <param name="numberOfPoints">Number of holding registers to read.</param>
+		/// <returns>Input registers status</returns>
 		public ushort[] ReadInputRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
 		{
 			return ReadRegisters(Modbus.ReadInputRegisters, slaveAddress, startAddress, numberOfPoints);
 		}
 
+		/// <summary>
+		/// Write a single coil value.
+		/// </summary>
+		/// <param name="slaveAddress">Address of the device to write to.</param>
+		/// <param name="coilAddress">Address to write value to.</param>
+		/// <param name="value">Value to write.</param>
 		public void WriteSingleCoil(byte slaveAddress, ushort coilAddress, bool value)
 		{
 			WriteSingleCoilRequestResponse request = new WriteSingleCoilRequestResponse(slaveAddress, coilAddress, value);
 			Transport.UnicastMessage<WriteSingleCoilRequestResponse>(request);
 		}
 
+		/// <summary>
+		/// Write a single holding register.
+		/// </summary>
+		/// <param name="slaveAddress">Address of the device to write to.</param>
+		/// <param name="registerAddress">Value to write.</param>
+		/// <param name="value">Value to write.</param>
 		public void WriteSingleRegister(byte slaveAddress, ushort registerAddress, ushort value)
 		{
 			WriteSingleRegisterRequestResponse request = new WriteSingleRegisterRequestResponse(slaveAddress, registerAddress, value);
 			Transport.UnicastMessage<WriteSingleRegisterRequestResponse>(request);
 		}
 
+		/// <summary>
+		/// Write a block of 1 to 123 contiguous registers.
+		/// </summary>
+		/// <param name="slaveAddress">Address of the device to write to.</param>
+		/// <param name="startAddress">Address to begin writing values.</param>
+		/// <param name="data">Values to write.</param>
 		public void WriteMultipleRegisters(byte slaveAddress, ushort startAddress, ushort[] data)
 		{
 			WriteMultipleRegistersRequest request = new WriteMultipleRegistersRequest(slaveAddress, startAddress, new RegisterCollection(data));
 			Transport.UnicastMessage<WriteMultipleRegistersResponse>(request);
 		}
 
+		/// <summary>
+		/// Force each coil in a sequence of coils to a provided value.
+		/// </summary>
+		/// <param name="slaveAddress">Address of the device to write to.</param>
+		/// <param name="startAddress">Address to begin writing values.</param>
+		/// <param name="data">Values to write.</param>
 		public void WriteMultipleCoils(byte slaveAddress, ushort startAddress, bool[] data)
 		{
 			WriteMultipleCoilsRequest request = new WriteMultipleCoilsRequest(slaveAddress, startAddress, new DiscreteCollection(data));
