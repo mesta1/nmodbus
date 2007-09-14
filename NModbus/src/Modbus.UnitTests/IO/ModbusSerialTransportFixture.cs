183d31386dba208cc6bebbbd69465424fa8264e2
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2007-09-14 23:06:03 +0000

    moved call to purge receive buffer for serial transport, added unit test.

--- NModbus/src/Modbus.UnitTests/IO/ModbusSerialTransportFixture.cs
+++ NModbus/src/Modbus.UnitTests/IO/ModbusSerialTransportFixture.cs
@@ -5,6 +5,8 @@ using Modbus.Message;
 using Modbus.UnitTests.Message;
 using Modbus.Utility;
 using NUnit.Framework;
+using Rhino.Mocks;
+using System;
 
 namespace Modbus.UnitTests.IO
 {
@@ -36,6 +38,47 @@ namespace Modbus.UnitTests.IO
 			ModbusAsciiTransport transport = new ModbusAsciiTransport();
 			transport.CheckFrame = false;
 			transport.CreateResponse<ReadCoilsInputsResponse>(new byte[] { 19, Modbus.ReadCoils, 0, 0, 0, 2, 115 });
+		}
+
+		/// <summary>
+		/// When using the serial RTU protocol the beginning of the message could get mangled leading to an unsupported message type.
+		/// We want to be sure to try the message again so clear the RX buffer and try again.
+		/// </summary>
+		[Test]
+		public void UnicastMessage_PurgeReceiveBuffer()
+		{
+			MockRepository mocks = new MockRepository();
+			ISerialResource serialResource = mocks.CreateMock<ISerialResource>();
+			ModbusSerialTransport transport = new ModbusRtuTransport(serialResource);
+
+			//transport.Write(null);
+			serialResource.DiscardInBuffer();
+			serialResource.Write(null, 0, 0);
+			LastCall.IgnoreArguments();
+
+			// mangled response
+			Expect.Call(serialResource.Read(new byte[] { 0, 0, 0, 0 }, 0, 4)).Return(4);
+
+			//transport.Write(null);
+			serialResource.DiscardInBuffer();
+			serialResource.Write(null, 0, 0);
+			LastCall.IgnoreArguments();
+
+			// read 4 coils from slave id 2
+			//Expect.Call(transport.ReadResponse<ReadCoilsInputsResponse>())
+			//    .Return(new ReadCoilsInputsResponse(Modbus.ReadCoils, 2, 1, new DiscreteCollection(true, false, true, false, false, false, false, false)));
+
+			// mangled response
+			Expect.Call(serialResource.Read(new byte[] { 0, 0, 0, 0 }, 0, 4)).Return(4);
+
+			mocks.ReplayAll();
+
+			ReadCoilsInputsRequest request = new ReadCoilsInputsRequest(Modbus.ReadCoils, 2, 3, 4);
+			ReadCoilsInputsResponse expectedResponse = new ReadCoilsInputsResponse(Modbus.ReadCoils, 2, 1, new DiscreteCollection(true, false, true, false, false, false, false, false));
+			ReadCoilsInputsResponse response = transport.UnicastMessage<ReadCoilsInputsResponse>(request);
+			//Assert.AreEqual(expectedResponse.MessageFrame, response.MessageFrame);
+
+			mocks.VerifyAll();
 		}
 	}
 }
