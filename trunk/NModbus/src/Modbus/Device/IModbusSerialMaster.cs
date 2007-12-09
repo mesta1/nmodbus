e470b51c592225551cff8313191887039f7974d0
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2007-12-09 00:49:11 +0000

    Refactored async operations in ModbusTcpSlave

--- trunk/NModbus/src/Modbus/Device/IModbusSerialMaster.cs
+++ trunk/NModbus/src/Modbus/Device/IModbusSerialMaster.cs
@@ -11,5 +11,15 @@ namespace Modbus.Device
 		/// Transport for used by this master.
 		/// </summary>
 		new ModbusSerialTransport Transport { get; }
+
+		/// <summary>
+		/// Serial Line only.
+		/// Diagnostic function which loops back the original data.
+		/// NModbus only supports looping back one ushort value, this is a limitation of the "Best Effort" implementation of the RTU protocol.
+		/// </summary>
+		/// <param name="slaveAddress">Address of device to test.</param>
+		/// <param name="data">Data to return.</param>
+		/// <returns>Return true if slave device echoed data.</returns>
+		bool ReturnQueryData(byte slaveAddress, ushort data);
 	}
 }
