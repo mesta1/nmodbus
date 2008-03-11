947147598142f5bf9260b88cc5893d082d175dd5
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2008-03-11 02:05:38 +0000

    updated version and changed System.Core.dll reference to copyLocal = false

--- trunk/NModbus/src/Modbus/Properties/AssemblyInfo.cs
+++ trunk/NModbus/src/Modbus/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleToAttribute("DynamicProxyGenAssembly2")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: GuidAttribute("a2ec5378-e1b7-4bb0-b696-1c657407eeb9")]
-[assembly: AssemblyVersionAttribute("1.5.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.5.0.0")]
+[assembly: AssemblyVersionAttribute("1.6.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.6.0.0")]
 
