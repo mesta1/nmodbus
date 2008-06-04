c175f82f3285d0b57e57dd4912c974c1912426f3
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2008-06-04 00:11:58 +0000

    NModbus uses unsigned types which are not CLS compliant, marking the assemblies as non CLS compliant.

--- trunk/NModbus/src/FtdAdapter/Properties/AssemblyInfo.cs
+++ trunk/NModbus/src/FtdAdapter/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Licensed under MIT License.")]
 [assembly: AssemblyDescription("Provides support for the FTD2XX USB driver.")]
 [assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
 [assembly: Guid("60741108-f477-476c-8139-c50ac621089f")]
 [assembly: AssemblyVersion("1.7.0.0")]
 [assembly: AssemblyFileVersion("1.7.0.0")]
