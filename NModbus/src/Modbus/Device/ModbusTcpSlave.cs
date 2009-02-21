364d008476c7edf63b7832a6c46d03ab1e42d917
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2009-02-21 23:58:29 +0000

    got rid of some warnings

--- NModbus/src/Modbus/Device/ModbusTcpSlave.cs
+++ NModbus/src/Modbus/Device/ModbusTcpSlave.cs
@@ -93,6 +93,10 @@ namespace Modbus.Device
 			}
 		}
 
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
