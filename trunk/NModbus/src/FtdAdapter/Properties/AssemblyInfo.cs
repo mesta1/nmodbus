80b95a662936684c8b025f833d46c80ae66a38a8
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2009-04-19 17:34:52 +0000

    setting com visible to true for vba applications

--- trunk/NModbus/src/FtdAdapter/Properties/AssemblyInfo.cs
+++ trunk/NModbus/src/FtdAdapter/Properties/AssemblyInfo.cs
@@ -6,7 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("NModbus")]
 [assembly: AssemblyCopyright("Licensed under MIT License.")]
 [assembly: AssemblyDescription("Provides support for the FTD2XX USB driver.")]
-[assembly: ComVisible(false)]
+// required for VBA applications
+[assembly: ComVisible(true)]
 [assembly: CLSCompliant(false)]
 [assembly: Guid("60741108-f477-476c-8139-c50ac621089f")]
 [assembly: AssemblyVersion("1.9.0.0")]
