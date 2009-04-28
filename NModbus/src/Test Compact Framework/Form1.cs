596f7272092a3a06c9f17fb11436125cb6c9ca88
sjalex <sjalex@97f8bd50-a619-0410-a575-7b5c649250ac>
2009-04-28 20:37:17 +0000

    created simple test app for compact framework

---
+++ NModbus/src/Test Compact Framework/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using Modbus.IntegrationTests;
+
+namespace Test_Compact_Framework
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			try
+			{
+				//textBox1.Text = "Testing serial...";
+				//TestCases.Serial();
+				textBox1.Text = "Testing TCP...";
+				TestCases.Tcp();
+				textBox1.Text = "Testing UDP...";
+				TestCases.Udp();
+
+				textBox1.Text = "Tests completed successfully.";
+			}
+			catch (Exception e)
+			{
+				textBox1.Text = e.ToString();
+			}
+		}
+	}
+}
